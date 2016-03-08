11a3d63b2a3a4048576c31dc57920bf30c096528
Ubuntu <arnell.megan@yahoo.com>
2016-03-08 05:38:17 +0000

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var fs = require('fs')
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('city-finder.html', { root: 'public' });
+});
+
+router.get('/getcity',function(req,res,next) {
+
+	fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+            if(err) throw err;
+            var cities = data.toString().split("\n");
+ 		
+	var myRe = new RegExp("^" + req.query.q);
+
+	var jsonresult = [];
+        for(var i = 0; i < cities.length; i++) {
+          var result = cities[i].search(myRe); 
+          if(result != -1) {
+           jsonresult.push({city:cities[i]});
+          } 
+        }   
+	res.status(200).json(jsonresult);
+ });
+
+
+          });
+
+
+
+module.exports = router;
